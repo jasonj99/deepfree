738ab080051ad77860a228ad7bf165c80969615e
fuzimaoxinan <475366898@qq.com>
2019-03-26 15:51:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,25 +2,18 @@
 # -*- coding: utf-8 -*- 
 
 from setuptools import setup, find_packages
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
- 
-with open('LICENSE') as fp:
-    license = fp.read()
   
 setup(name = "deepfree",
-      version = "0.1.11",
+      version = "0.2.2",
       author = "Zhuofu Pan",
       author_email = "475366898@qq.com",
       description = "keras-style deep network package for classification and prediction",
-      long_description=long_description,
+      long_description=open("README.md", encoding='utf-8').read(),
       long_description_content_type="text/markdown",
       keywords = ("deep network", "deep learning", "DBN", "SAE"),
       url = "http://github.com/fuzimaoxinan/deepfree",
       maintainer='Zhuofu Pan',
       maintainer_email="475366898@qq.com",
-      license=license,
       packages = find_packages(),
       install_requires = ['tensorflow>=1.10.0' ],
       platforms=['any'],
@@ -29,4 +22,7 @@ setup(name = "deepfree",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
       ],
-      )
+      data_files=["README.md"],
+      #include_package_data = True,
+      #package_data = {'deepfree': ['dataset/MNIST_data/*']}
+)
